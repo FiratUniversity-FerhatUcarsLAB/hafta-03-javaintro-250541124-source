cd7ef68f40809e8b4a53dbb7e6a499a6b96abbe5
faik dursun <250541124@firat.edu.tr>
2025-10-23 17:39:04 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/dairehesapı.java
@@ -0,0 +1,17 @@
+public class dairehesapı {
+    public static void main(String[] args) {
+        double r = 5.5 ;
+        double pi = 3.14;
+
+        // Alan hesapla
+        double alan = pi*r*r;
+
+        // Cevre Hesapla
+        double cevre = 2*pi*r;
+
+        // Sonucları hesapla
+        System.out.println("yariçap: " + r);
+        System.out.println("alan: " + alan);
+        System.out.println("cevre: "  +cevre);
+    }
+}
---
+++ Task-3-GeometrikHesaplamalar/dikdörtgen.java
@@ -0,0 +1,15 @@
+public class dikdörtgen {
+    public static void main(String[] args) {
+        double uzunkenar  = 7.9 ;
+        double  kısakenar = 4.5 ;
+
+        double alan = uzunkenar*kısakenar;
+        double cevre = 2*(uzunkenar+kısakenar);
+
+        System.out.println("uzunkenar = " + uzunkenar);
+        System.out.println("kısa kenar"+kısakenar);
+        System.out.println("dikdörtgen alanı"+alan);
+        System.out.println("cwvre"+cevre);
+
+    }
+}
